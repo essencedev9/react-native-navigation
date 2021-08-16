4c4c7bf6bc8513aab243647869b67582aa10ebe1
essencedev9 <essencedev9@nntuple.com>
2021-08-16 16:45:34 +0900

    Add Function - 헤더 수정

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Home, List, Chat } from '../screens';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 const StackNav = () => {
   return (
     <Stack.Navigator
       initialRouteName="Home"
-      screenOptions={{ cardStyle: { backgroundColor: '#ffffff' } }}
+      screenOptions={{
+        cardStyle: { backgroundColor: '#ffffff' },
+        headerStyle: {
+          height: 120,
+          backgroundColor: '#555555',
+          borderBottomWidth: 5,
+          borderBottomColor: '#111111',
+        },
+        headerTitleStyle: {
+          fontSize: 24,
+          color: '#ffffff',
+        },
+        headerTitleAlign: 'center',
+        headerTitle: ({ style }) => {
+          return <FontAwesome5 name="react" style={style} />;
+        },
+      }}
     >
       <Stack.Screen
         name="Home"
         component={Home}
-        options={{ cardStyle: { backgroundColor: '#ffffff' } }}
+        options={{ headerShown: false }}
       />
       <Stack.Screen name="List" component={List} />
-      <Stack.Screen name="Chat" component={Chat} />
+      <Stack.Screen
+        name="Chat"
+        component={Chat}
+        options={{ headerTitle: 'Chat Screen' }}
+      />
     </Stack.Navigator>
   );
 };
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import Button from '../components/Button';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Container = styled.View`
   align-items: center;
   /* background-color: #ffffff; */
+  padding-top: ${({ insets: { top } }) => top}px;
+  padding-left: ${({ insets: { left } }) => left}px;
+  padding-right: ${({ insets: { right } }) => right}px;
+  padding-bottom: ${({ insets: { bottom } }) => bottom}px;
 `;
 const StyledText = styled.Text`
   font-size: 30px;
@@ -12,8 +17,10 @@ const StyledText = styled.Text`
 `;
 
 const Home = ({ navigation }) => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <Container>
+    <Container insets={insets}>
       <StyledText>Home</StyledText>
       <Button title="List" onPress={() => navigation.navigate('List')} />
     </Container>
